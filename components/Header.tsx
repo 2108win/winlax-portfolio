b3e1ac4174cd97bae52eeffaf695e5436ddd2cd9
Win Lã <dangcapcuawin@gmail.com>
2023-12-17 18:05:07 +0700

    update link CV

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,6 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 import { Link, animateScroll as scroll } from "react-scroll";
 import React from "react";
-import { SocialIcon } from "react-social-icons";
 import ThemeToggler from "../src/hooks/ThemeToggler";
 type Props = {};
 
@@ -29,12 +28,6 @@ export default function Header({}: Props) {
           <Image className="cursor-pointer" src="/logo.png" width={50} height={50} alt="logo" />
         </Link>
         <ThemeToggler />
-        {/* Social Icon */}
-        {/* <SocialIcon url="https://www.facebook.com/2108win/" fgColor="gray" bgColor="transparent" />
-        <SocialIcon url="https://www.instagram.com/win_lax/" fgColor="gray" bgColor="transparent" />
-        <SocialIcon url="https://github.com/2108win" fgColor="gray" bgColor="transparent" /> */}
-        {/* icon dark mode */}
-        {/* <Switcher /> */}
       </motion.div>
 
       <motion.a
@@ -52,7 +45,7 @@ export default function Header({}: Props) {
           duration: 1,
         }}
         className="buttonMain"
-        href="https://www.topcv.vn/xem-cv/AVIDAVNSUwINAFMBUgRTDQNcAA9XVABdVlUAVQ4c53"
+        href="https://www.topcv.vn/xem-cv/VVEDAABTBlBVAA9TUVMFVQBXVAFWAAFWDANWUAb4bf"
         download
       >
         Download CV
